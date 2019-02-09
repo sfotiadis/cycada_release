c7abe095152703581854971ebb24a82ec741722b
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-02-09 16:12:05 +0000

    changes

--- reqs_conda.sh
+++ reqs_conda.sh
@@ -1,3 +1,4 @@
+conda install scikit-learn
 conda install -c anaconda scipy 
 conda install -c pytorch torchvision 
 conda install -c conda-forge tensorboardx tensorflow tqdm click requests colorlog pyyaml
